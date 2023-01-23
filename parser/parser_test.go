bcfe4969ffd4b7730a53b2e40cc519262fcf66d2
Joshua Humphries <2035234+jhump@users.noreply.github.com>
2023-01-23 13:41:52 -0500

    Fix another fuzz test timeout (#90)
    The fuzz test in question constructed a pathological input case whose
    performance is unexpectedly terrible. The issue is actually with huge,
    but invalid string literals. The issue was actually introduced recently
    with #78. The way that was resolved was to accumulate _all escape
    errors_ inside the string literal and then report them at the end. It turns
    out, this is needlessly expensive if, for example, there are 1000s of
    errors inside a string literal, but the configured error reporter only cares
    about the first (e.g. it fails fast). At that point, the cost of constructing the
    1000s of errors and computing their source position info is wasteful and
    is what the profiler found to be the issue in this test case.
    
    So now, instead of accumulating all of them and then reporting them at
    the end, we report them as we go, and simply skip reporting (and skip
    creation of error and computation of source position) if the handler is
    done (e.g. it returns a non-nil error when handling one of the escape
    errors).

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -296,39 +296,53 @@ func readerForTestdata(t testing.TB, filename string) io.Reader {
 func TestPathological(t *testing.T) {
 	t.Parallel()
 
-	// This test verifies that the test case found in fuzz tests has
+	// This test verifies that the test cases found in fuzz tests have
 	// adequate performance.
 	//   https://oss-fuzz.com/testcase-detail/4766256800858112
-
-	ctx, cancel := context.WithCancel(context.Background())
-	// Fuzz testing complains if this loop, with 100 iterations, takes longer
-	// than 60 seconds. To prevent this test from being too slow, we limit to
-	// 3 iterations and no longer than 1 second (which is a stricter deadline).
-	allowedDuration := time.Second
-	if isRace {
-		// We increase that threshold to 10 seconds when the race detector is enabled.
-		// The race detector has been observed to make it take ~8x as long. If coverage
-		// is *also* enabled, the test can take 19x as long(!!). But 10s should still
-		// be a reasonable limit in practice. (Unfortunately, there doesn't appear to
-		// be a way to easily detect if coverage is enabled.)
-		allowedDuration = 10 * time.Second
-		t.Logf("allowing %v since race detector is enabled", allowedDuration)
+	//   https://oss-fuzz.com/testcase-detail/4952577018298368
+	testCases := map[string]bool{
+		"pathological.proto":  true,
+		"pathological2.proto": false,
 	}
-	timer := time.AfterFunc(allowedDuration, func() {
-		require.Fail(t, "test took too long to execute (> %v)", allowedDuration)
-		cancel()
-	})
-	defer timer.Stop()
+	for fileName := range testCases {
+		fileName, canParse := fileName, testCases[fileName] // don't want test func below to capture loop var
+		t.Run(fileName, func(t *testing.T) {
+			t.Parallel()
+			ctx, cancel := context.WithCancel(context.Background())
+			// Fuzz testing complains if this loop, with 100 iterations, takes longer
+			// than 60 seconds. To prevent this test from being too slow, we limit to
+			// 3 iterations and no longer than 1 second (which is a stricter deadline).
+			allowedDuration := time.Second
+			if isRace {
+				// We increase that threshold to 10 seconds when the race detector is enabled.
+				// The race detector has been observed to make it take ~8x as long. If coverage
+				// is *also* enabled, the test can take 19x as long(!!). But 10s should still
+				// be a reasonable limit in practice. (Unfortunately, there doesn't appear to
+				// be a way to easily detect if coverage is enabled.)
+				allowedDuration = 10 * time.Second
+				t.Logf("allowing %v since race detector is enabled", allowedDuration)
+			}
+			timer := time.AfterFunc(allowedDuration, func() {
+				require.Fail(t, "test took too long to execute (> %v)", allowedDuration)
+				cancel()
+			})
+			defer timer.Stop()
 
-	for i := 0; i < 3; i++ {
-		if ctx.Err() != nil {
-			break
-		}
-		r := readerForTestdata(t, "pathological.proto")
-		handler := reporter.NewHandler(nil)
-		fileNode, err := Parse("pathological.proto", r, handler)
-		require.NoError(t, err)
-		_, err = ResultFromAST(fileNode, true, handler)
-		require.NotNil(t, err)
+			for i := 0; i < 3; i++ {
+				if ctx.Err() != nil {
+					break
+				}
+				r := readerForTestdata(t, fileName)
+				handler := reporter.NewHandler(nil)
+				fileNode, err := Parse(fileName, r, handler)
+				if canParse {
+					require.NoError(t, err)
+					_, err = ResultFromAST(fileNode, true, handler)
+					require.NotNil(t, err)
+				} else {
+					require.NotNil(t, err)
+				}
+			}
+		})
 	}
 }
