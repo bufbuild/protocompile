169e8b1c6099a7cdf8d942e233c11a992bc118ea
Miguel Young <myoung@buf.build>
2024-12-05 13:14:15 -0500

    Add package internal/intern (#379)
    This PR adds a simple interning table with 32-bit IDs. It currently
    implements interning using a slice plus a reverse index, protected by a
    read-write lock. This will be used so that qualified Protobuf names can
    be represented as slices: `name []intern.ID`.
    
    The table supports up to two billion interned names, which is unlikely
    to ever be overflown by a single complier session. Also, ASCII
    identifiers up to five characters (which may contain, but not end with,
    a period) are encoded directly in the ID using the LLVM bitcode `char6`
    encoding. This removes the need to hit the reverse index for very small
    common identifiers, such as "data", "value", "error", "bytes", and
    "msg".
    
    It may be worthwhile to investigate in the future whether using 64-bit
    IDs (or some `[n]uint64`), which would allow identifiers with at most
    `64 / 6 ~ 10` characters to be encoded inline. Whether this is worth
    doing will depend on what most common identifier lengths on the BSR are.

---
+++ internal/intern/intern_test.go
@@ -0,0 +1,81 @@
+// Copyright 2020-2024 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// package intern provides an interning table abstraction to optimize symbol
+// resolution.
+package intern_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/bufbuild/protocompile/internal/intern"
+)
+
+func TestIntern(t *testing.T) {
+	t.Parallel()
+
+	data := []string{
+		"",
+		"a",
+		"abc",
+		"?",
+		"xy.z",
+		"a_b_c",
+		".....",
+		"foo.",
+		"foo.a",
+		"very long",
+		" ",
+		"verylong",
+	}
+
+	var table intern.Table
+	for i := 0; i < 3; i++ {
+		for _, s := range data {
+			s := s
+
+			t.Run(fmt.Sprintf("%s/%d", s, i), func(t *testing.T) {
+				t.Parallel()
+
+				id := table.Intern(s)
+				assert.Equal(t, s, table.Value(id), "id: %v", id)
+				assert.Equal(t, shouldInline(s), id < 0)
+			})
+		}
+	}
+}
+
+func shouldInline(s string) bool {
+	if s == "" || len(s) > 5 || strings.HasSuffix(s, ".") {
+		return false
+	}
+
+	for _, r := range s {
+		switch {
+		case r >= '0' && r <= '9',
+			r >= 'a' && r <= 'z',
+			r >= 'A' && r <= 'Z',
+			r == '_', r == '.':
+
+		default:
+			return false
+		}
+	}
+
+	return true
+}
