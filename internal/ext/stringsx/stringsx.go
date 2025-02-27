3a19ea5995b5dc52a3513349154d7bdca2d9c634
Miguel Young <myoung@buf.build>
2025-02-26 17:20:29 -0800

    Add various new standard library extensions (#457)
    This PR aggregates new additions to the `exts` directory form various
    in-progress PRs, aiming to try to get those changes out of in-progress
    PRs and into their own, easier-to-review PR.

--- internal/ext/stringsx/stringsx.go
+++ internal/ext/stringsx/stringsx.go
@@ -17,16 +17,46 @@ package stringsx
 
 import (
 	"strings"
+	"unicode/utf8"
 	"unsafe"
 
 	"github.com/bufbuild/protocompile/internal/ext/iterx"
+	"github.com/bufbuild/protocompile/internal/ext/slicesx"
 	"github.com/bufbuild/protocompile/internal/ext/unsafex"
 	"github.com/bufbuild/protocompile/internal/iter"
 )
 
+// Rune returns the rune at the given byte index.
+//
+// Returns 0, false if out of bounds. Returns U+FFFD, false if rune decoding fails.
+func Rune[I slicesx.SliceIndex](s string, idx I) (rune, bool) {
+	if !slicesx.BoundsCheck(idx, len(s)) {
+		return 0, false
+	}
+	r, _ := utf8.DecodeRuneInString(s[idx:])
+	return r, r != utf8.RuneError
+}
+
+// Rune returns the previous rune at the given byte index.
+//
+// Returns 0, false if out of bounds. Returns U+FFFD, false if rune decoding fails.
+func PrevRune[I slicesx.SliceIndex](s string, idx I) (rune, bool) {
+	if !slicesx.BoundsCheck(idx-1, len(s)) {
+		return 0, false
+	}
+
+	r, _ := utf8.DecodeLastRuneInString(s[:idx])
+	return r, r != utf8.RuneError
+}
+
+// Byte returns the rune at the given index.
+func Byte[I slicesx.SliceIndex](s string, idx I) (byte, bool) {
+	return slicesx.Get(unsafex.BytesAlias[[]byte](s), idx)
+}
+
 // EveryFunc verifies that all runes in the string satisfy the given predicate.
 func EveryFunc(s string, p func(rune) bool) bool {
-	return iterx.All(Runes(s), p)
+	return iterx.Every(Runes(s), p)
 }
 
 // Runes returns an iterator over the runes in a string.
