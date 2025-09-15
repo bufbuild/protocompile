37463e132ae1aac02701ea99928e7261cf5f44fc
Miguel Young <myoung@buf.build>
2025-09-15 15:20:14 -0700

    Implement the rest of field number handling (#543)
    This PR adds field number, extension range, and reserved range handling
    and overlap checking. This is a very complicated process to get right,
    because there's many places with potential quadratic behavior, and
    generating good diagnostics is tricky.
    
    The strategy I've taken is to implement an "interval intersection set",
    which is an interval multimap which allows querying for the intersection
    of all intervals which contain a point. This means that we can fill the
    map with all of the tag intervals, and then iterate over all of the
    entries: disjoint intersections of the elements in the map.
    
    This means, for example, that we can diagnose all overlapping ranges
    (treating field numbers as a one-element range), point out which range
    caused the overlap, and check that extension numbers match an extension
    range, all in O(n log n)[^1] time.
    
    This PR adds extensive tests to check that all sorts of cases are
    correctly diagnosed. Eventually, I would like to build "field number
    suggestion" based on the data structure added in this PR. It also
    contains tests for the interval map.
    
    This PR also includes lowering extension/reserved ranges out of AST and
    into IR. It also includes an updated API for working with these that is
    more conducive to the API the interval data structure wants to have.
    
    One thing not included is extension number overlap checking. This
    requires knowing *all* input files, and then calculating a set of all
    extension numbers for every type. That will get implemented later, and
    will require a new type of `Opener` (something that lists every
    entry-point file).
    
    [^1]: There is one case where we can get quadratic behavior. This is
    because inserting to an `interval.Intersect` is actually O(k log n),
    where k is the number of intersecting intervals. No valid Protobuf file
    can trigger this behavior, but pathological files can, such as the
    fragment `reserved 1, 1 to 2, 1 to 3, 1 to 4, ...`, which forces k = n.
    This can be mitigated by not processing more ranges after we see N
    overlaps (for some constant N). This ensures k = O(N).

---
+++ experimental/ir/lower_numbers.go
@@ -0,0 +1,337 @@
+// Copyright 2020-2025 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ir
+
+import (
+	"github.com/bufbuild/protocompile/experimental/ast"
+	"github.com/bufbuild/protocompile/experimental/internal/taxa"
+	"github.com/bufbuild/protocompile/experimental/report"
+	"github.com/bufbuild/protocompile/experimental/seq"
+	"github.com/bufbuild/protocompile/internal/arena"
+	"github.com/bufbuild/protocompile/internal/ext/iterx"
+)
+
+// evaluateFieldNumbers evaluates all non-extension field numbers: that is,
+// the numbers in reserved ranges and in non-extension field and enum value
+// declarations.
+func evaluateFieldNumbers(f File, r *report.Report) {
+	for ty := range seq.Values(f.AllTypes()) {
+		scope := ty.FullName()
+
+		var kind memberNumber
+		switch {
+		case ty.IsEnum():
+			kind = enumNumber
+		case ty.IsMessageSet():
+			kind = messageSetNumber
+		default:
+			kind = fieldNumber
+		}
+
+		for member := range seq.Values(ty.Members()) {
+			n, ok := evaluateMemberNumber(f.Context(), scope, member.AST().Value(), kind, false, r)
+			if ok {
+				member.raw.number = n
+			}
+		}
+
+		for _, raw := range ty.raw.ranges {
+			tags := ReservedRange{ty.withContext, ty.Context().arenas.ranges.Deref(raw)}
+
+			switch tags.AST().Kind() {
+			case ast.ExprKindRange:
+				a, b := tags.AST().AsRange().Bounds()
+
+				start, startOk := evaluateMemberNumber(f.Context(), scope, a, kind, false, r)
+				end, endOk := evaluateMemberNumber(f.Context(), scope, b, kind, true, r)
+
+				if !startOk || !endOk {
+					continue
+				}
+
+				if start > end {
+					what := taxa.Reserved
+					if tags.ForExtensions() {
+						what = taxa.Extensions
+					}
+
+					r.Errorf("empty %s %v %v", what).Apply(
+						report.Snippet(tags.AST()),
+						report.Notef("range syntax requires that start <= end"),
+					)
+					continue
+				}
+
+				if start == end {
+					r.Warnf("singleton range can be simplified").Apply(
+						report.Snippet(tags.AST()),
+						report.SuggestEdits(tags.AST(), "replace with a single number", report.Edit{
+							Start: 0, End: tags.AST().Span().Len(),
+							Replace: a.Span().Text(),
+						}),
+					)
+				}
+
+				tags.raw.first = start
+				tags.raw.last = end
+
+			default:
+				n, ok := evaluateMemberNumber(f.Context(), scope, tags.AST(), kind, false, r)
+				if !ok {
+					continue
+				}
+
+				tags.raw.first = n
+				tags.raw.last = n
+			}
+		}
+	}
+
+	for extn := range seq.Values(f.AllExtensions()) {
+		var kind memberNumber
+		switch {
+		case extn.Container().IsMessageSet():
+			kind = messageSetNumber
+		default:
+			kind = fieldNumber
+		}
+
+		scope := extn.Context().File().Package()
+		if ty := extn.Parent(); !ty.IsZero() {
+			scope = ty.FullName()
+		}
+
+		extn.raw.number, _ = evaluateMemberNumber(f.Context(), scope, extn.AST().Value(), kind, false, r)
+	}
+}
+
+func evaluateMemberNumber(c *Context, scope FullName, number ast.ExprAny, kind memberNumber, allowMax bool, r *report.Report) (int32, bool) {
+	if number.IsZero() {
+		return 0, false // Diagnosed for us elsewhere.
+	}
+
+	e := &evaluator{
+		Context: c,
+		Report:  r,
+		scope:   scope,
+	}
+
+	// Don't bother allocating a whole Value for this.
+	v, ok := e.evalBits(evalArgs{
+		expr:         number,
+		memberNumber: kind,
+		allowMax:     allowMax,
+	})
+
+	return int32(v), ok
+}
+
+// buildFieldNumberRanges builds the field number range table for all types.
+//
+// This also checks for and diagnoses overlaps.
+func buildFieldNumberRanges(f File, r *report.Report) {
+	// overlapLimit sets the maximum number of overlapping ranges we tolerate
+	// before we stop processing overlapping ranges.
+	//
+	// Inserting to an [interval.Intersect] is actually O(k log n), where k is
+	// the number of intersecting intervals. No valid Protobuf file can trigger
+	// this behavior, but pathological files can, such as the fragment
+	//
+	//	reserved 1, 1 to 2, 1 to 3, 1 to 4, ...
+	//
+	// which forces k = n. This can be mitigated by not processing more ranges
+	// after we see N overlaps This ensures k = O(N). For example, in the
+	// example above, we would only have ranges up to 1 to N before stopping,
+	// and so we get at worst performance O(N^2 log n) = O(log n), because N
+	// is a constant. Note processing members still proceeds, and is still
+	// O(n log n) work (because k <= 1 in this case).
+	//
+	// This is that N. In this case, we set missingRanges so that extension
+	// range checks are skipped, to avoid generating incorrect diagnostics (the
+	// file will already be rejected in this case, so not providing precise
+	// diagnostics in is fine).
+	const overlapLimit = 50
+
+	// First, dump all of the ranges into the intersection set.
+	for ty := range seq.Values(f.AllTypes()) {
+		var totalOverlaps int
+		for tagRange := range iterx.Chain(
+			seq.Values(ty.ReservedRanges()),
+			seq.Values(ty.ExtensionRanges()),
+		) {
+			lo, hi := tagRange.Range()
+			if lo == 0 || hi == 0 {
+				continue // Diagnosed already.
+			}
+			disjoint := ty.raw.rangesByNumber.Insert(lo, hi, rawTagRange{
+				ptr: arena.Untyped(f.Context().arenas.ranges.Compress(tagRange.raw)),
+			})
+
+			// Avoid quadratic behavior. See overlapLimit's comment above.
+			if !disjoint {
+				totalOverlaps++
+				if totalOverlaps > overlapLimit {
+					ty.raw.missingRanges = true
+					break
+				}
+			}
+		}
+
+		// Members last, so that if an intersection contains only members, the
+		// first value is a member.
+		for member := range seq.Values(ty.Members()) {
+			n := member.Number()
+			if n == 0 {
+				continue // Diagnosed already.
+			}
+			ty.raw.rangesByNumber.Insert(n, n, rawTagRange{
+				isMember: true,
+				ptr:      arena.Untyped(f.Context().arenas.members.Compress(member.raw)),
+			})
+		}
+
+		// Now, iterate over every entry and diagnose the ones that have more
+		// than one value.
+		for entry := range ty.raw.rangesByNumber.Entries() {
+			if len(entry.Values) < 2 {
+				continue
+			}
+
+			first := TagRange{ty.withContext, entry.Values[0]}
+
+			for _, tags := range entry.Values[1:] {
+				tags := TagRange{ty.withContext, tags}
+				if a, b := first.AsMember(), tags.AsMember(); ty.AllowsAlias() && !a.IsZero() && !b.IsZero() {
+					continue
+				}
+
+				r.Error(errOverlap{
+					ty:     ty,
+					first:  first,
+					second: tags,
+				})
+			}
+		}
+	}
+
+	// Check that every extension has a corresponding extension range.
+extensions:
+	for extn := range seq.Values(f.AllExtensions()) {
+		n := extn.Number()
+		if n == 0 {
+			continue // Diagnosed already.
+		}
+
+		ty := extn.Container()
+		if ty.IsZero() || !ty.IsMessage() {
+			continue
+		}
+
+		var first TagRange
+		for tags := range ty.Ranges(n) {
+			if first.IsZero() {
+				first = tags
+			}
+
+			if tags.AsReserved().ForExtensions() {
+				continue extensions
+			}
+		}
+
+		var d *report.Diagnostic
+		if !first.IsZero() {
+			d = r.Error(errOverlap{
+				ty:     ty,
+				first:  first,
+				second: extn.AsTagRange(),
+			})
+		} else {
+			// Don't diagnose if we're missing some ranges, because we might
+			// produce false positives. This can only happen for types that have
+			// already generated diagnostics, so it's ok to skip diagnosing.
+			if ty.raw.missingRanges {
+				continue
+			}
+
+			d = r.Errorf("extension with unreserved number `%v`", n).Apply(
+				report.Snippet(extn.AST().Value()),
+			)
+		}
+		d.Apply(report.Helpf(
+			"the parent message `%s` must have reserved this number with an %s, e.g. `extensions %v;`",
+			ty.FullName(), taxa.Extensions, extn.Number(),
+		))
+	}
+
+	// NOTE: Can't do extension number overlap checking yet, because we need
+	// a global view of all files to do that.
+}
+
+type errOverlap struct {
+	ty            Type
+	first, second TagRange
+}
+
+func (e errOverlap) Diagnose(d *report.Diagnostic) {
+	what := taxa.FieldNumber
+	if e.ty.IsEnum() {
+		what = taxa.EnumValue
+	}
+
+again:
+	if second := e.second.AsMember(); !second.IsZero() {
+		if first := e.first.AsMember(); !first.IsZero() {
+			d.Apply(
+				report.Message("%v `%v` used more than once", what, second.Number()),
+				report.Snippetf(second.AST().Value(), "used here"),
+				report.Snippetf(first.AST().Value(), "previously used here"),
+			)
+		} else {
+			first := e.first.AsReserved()
+			d.Apply(
+				report.Message("use of reserved %v `%v`", what, second.Number()),
+				report.Snippetf(second.AST().Value(), "used here"),
+				report.Snippetf(first.AST(), "%v reserved here", what),
+			)
+		}
+	} else {
+		if first := e.first.AsMember(); !first.IsZero() {
+			e.second, e.first = e.first, e.second
+			goto again
+		}
+
+		second := e.second.AsReserved()
+		first := e.first.AsReserved()
+
+		lo1, hil := first.Range()
+		lo2, hi2 := second.Range()
+		d.Apply(
+			report.Message("overlapping %v ranges", what),
+			report.Snippetf(second.AST(), "this range"),
+			report.Snippetf(first.AST(), "overlaps with this one"),
+		)
+
+		lo1 = max(lo1, lo2)
+		hil = min(hil, hi2)
+		if lo1 == hil {
+			d.Apply(report.Helpf("they overlap at `%v`", lo1))
+		} else {
+			d.Apply(report.Helpf("they overlap in the range `%v to %v`", lo1, hil))
+		}
+
+		// TODO: Generate a suggestion to split the range, if both ranges are
+		// of the same type.
+	}
+}
---
+++ internal/interval/intersect.go
@@ -0,0 +1,238 @@
+// Copyright 2020-2025 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package interval
+
+import (
+	"fmt"
+	"iter"
+	"slices"
+
+	"github.com/tidwall/btree"
+	"golang.org/x/exp/constraints" //nolint:exptostd // Tries to replace w/ cmp.
+)
+
+// Intersection is an interval intersection map: a collection of intervals,
+// such that given a point in K, one can query for the intersection of all
+// intervals in the collection which contain it, along with the values
+// associated with each of those intervals.
+//
+// A zero value is ready to use.
+type Intersect[K Endpoint, V any] struct {
+	// Keys in this map are the ends of intervals in the map.
+	tree    btree.Map[K, *Entry[K, V]]
+	pending []*Entry[K, V] // Scratch space for Insert().
+}
+
+// Endpoint is a type that may be used as an interval endpoint.
+type Endpoint = constraints.Integer
+
+// Entry is an entry in a [Intersect]. This means that it is the intersection
+// of all intervals which contain a particular point.
+type Entry[K Endpoint, V any] struct {
+	Start, End K   // The interval range, inclusive.
+	Values     []V // Values from intervals that contain the query point, in insertion order.
+}
+
+// Contains returns whether an entry contains a given point.
+func (e Entry[K, V]) Contains(point K) bool {
+	return e.Start <= point && point <= e.End
+}
+
+// Get returns the intersection of all intervals which contain point.
+//
+// If no such interval exists, the [Entry].Values will be nil.
+func (m *Intersect[K, V]) Get(point K) Entry[K, V] {
+	iter := m.tree.Iter()
+	found := iter.Seek(point)
+
+	if !found || point < iter.Value().Start {
+		// Check that the interval actually contains key. It is implicit
+		// already that key <= end.
+		return Entry[K, V]{}
+	}
+
+	return *iter.Value()
+}
+
+// Entries returns an iterator over the entries in this map.
+//
+// There exists one entry per maximal subset of the map with non-empty
+// intersection. Entries are yielded in order, and are pairwise disjoint.
+func (m *Intersect[K, V]) Entries() iter.Seq[Entry[K, V]] {
+	return func(yield func(Entry[K, V]) bool) {
+		iter := m.tree.Iter()
+		for more := iter.First(); more; more = iter.Next() {
+			if !yield(*iter.Value()) {
+				return
+			}
+		}
+	}
+}
+
+// Insert inserts a new interval into this map, with the given associated value.
+// Both endpoints are inclusive.
+//
+// Returns true if the interval was disjoint from all others in the set.
+func (m *Intersect[K, V]) Insert(start, end K, value V) (disjoint bool) {
+	if start > end {
+		panic(fmt.Sprintf("interval: start (%#v) > end (%#v)", start, end))
+	}
+
+	var prev *Entry[K, V]
+	for entry := range m.intersect(start, end) {
+		if prev == nil && start < entry.Start {
+			// Need to insert an extra entry for the stuff between start and the
+			// first interval.
+			m.pending = append(m.pending, &Entry[K, V]{
+				Start:  start,
+				End:    entry.Start - 1,
+				Values: []V{value},
+			})
+		}
+
+		// Make a copy of entry.Values; entry.Values may get modified in a way
+		// where appending to it results in value appearing twice.
+		//
+		// NB: the values array may be shared across different entries where one
+		// is a prefix of the other.
+		orig := entry.Values
+
+		// If the entry contains end, we need to split it at end.
+		if entry.Contains(end) && end < entry.End {
+			next := &Entry[K, V]{
+				Start:  entry.Start,
+				End:    end,
+				Values: append(slices.Clip(orig), value),
+			}
+
+			// Shorten the existing entry.
+			entry.Start = end + 1
+
+			// Add next to the pending queue and use it as the entry here
+			// onwards.
+			m.pending = append(m.pending, next)
+			entry = next
+		}
+
+		// If the entry contains start, we also need to split it.
+		if entry.Contains(start) && entry.Start < start {
+			next := &Entry[K, V]{
+				Start:  entry.Start,
+				End:    start - 1,
+				Values: orig,
+			}
+
+			// Add next to the pending queue, but *don't* use it as entry,
+			// because it does not overlap!
+			m.pending = append(m.pending, next)
+
+			// Shorten the existing entry (this one overlaps [a, b]).
+			entry.Start = start
+		}
+
+		// Add the value to this overlap.
+		//nolint:gocritic // Slice assignment false positive.
+		entry.Values = append(orig, value)
+
+		if prev != nil && prev.End < entry.Start {
+			// Add a new interval in between this one and the previous.
+			m.pending = append(m.pending, &Entry[K, V]{
+				Start:  prev.End + 1,
+				End:    entry.Start - 1,
+				Values: []V{value},
+			})
+		}
+
+		prev = entry
+	}
+
+	if prev != nil && prev.End < end {
+		// Need to insert an extra entry for the stuff between the
+		// last interval and end.
+		m.pending = append(m.pending, &Entry[K, V]{
+			Start:  prev.End + 1,
+			End:    end,
+			Values: []V{value},
+		})
+	}
+
+	for _, entry := range m.pending {
+		m.tree.Set(entry.End, entry)
+	}
+	m.pending = m.pending[:0]
+
+	if prev == nil {
+		m.tree.Set(end, &Entry[K, V]{
+			Start:  start,
+			End:    end,
+			Values: []V{value},
+		})
+	}
+
+	return prev == nil
+}
+
+// Format implements [fmt.Formatter].
+func (m *Intersect[K, V]) Format(s fmt.State, v rune) {
+	fmt.Fprint(s, "{")
+	first := true
+	m.tree.Scan(func(end K, entry *Entry[K, V]) bool {
+		if !first {
+			fmt.Fprint(s, ", ")
+		}
+		first = false
+
+		if entry.Start == end {
+			fmt.Fprintf(s, "%#v: ", entry.Start)
+		} else {
+			fmt.Fprintf(s, "[%#v, %#v]: ", entry.Start, end)
+		}
+		fmt.Fprintf(s, fmt.FormatString(s, v), entry.Values)
+
+		return true
+	})
+	fmt.Fprint(s, "}")
+}
+
+// intersect returns an iterator over the intervals that intersect [start, end].
+func (m *Intersect[K, V]) intersect(start, end K) iter.Seq[*Entry[K, V]] {
+	return func(yield func(*Entry[K, V]) bool) {
+		// Here, [a, b] is the query interval, and [c, d] is the current
+		// interval we're looking at.
+		a, b := start, end
+		iter := m.tree.Iter()
+
+		// We need to walk the tree forwards, finding overlapping intervals,
+		// until we find an interval that contains b, is greater than b, or we
+		// reach the end of the tree.
+		//
+		// This logic conveniently handles the case where the map is empty,
+		// and when [a, b] is greater than all other intervals, because Seek()
+		// will return false.
+		for more := iter.Seek(a); more; more = iter.Next() {
+			c, _ := iter.Value().Start, iter.Value().End
+
+			// By construction, we already know that a <= d, so if [c, d]
+			// is less than b or contains it, it intersects [a, b].
+			//
+			// If b is less than [c, d], we're already done, so we don't
+			// need to yield this value. yield() is only called when !(b < c),
+			// i.e. c <= b.
+			if b < c || !yield(iter.Value()) {
+				return
+			}
+		}
+	}
+}
