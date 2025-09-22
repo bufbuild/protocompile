39be2ca08674480c61380946e47ed0257bbc4cf9
Miguel Young de la Sota <myoung@buf.build>
2025-09-22 14:37:37 -0700

    lint

--- experimental/ir/builtins.go
+++ experimental/ir/builtins.go
@@ -1,3 +1,17 @@
+// Copyright 2020-2025 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ir
 
 import (
@@ -68,7 +82,7 @@ func resolveBuiltins(c *Context) {
 	ids := reflect.ValueOf(c.session.builtins)
 	for i := range v.NumField() {
 		field := v.Field(i)
-		id := ids.FieldByName(v.Type().Field(i).Name).Interface().(intern.ID)
+		id := ids.FieldByName(v.Type().Field(i).Name).Interface().(intern.ID) //nolint:errcheck
 		kind := kinds[field.Type()]
 
 		ref := c.exported.lookup(c, id)
