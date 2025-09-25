c00d67d22b2c2cb8f67d346e54e8ece9cb09baea
Edward McFarlane <emcfarlane@buf.build>
2025-09-25 18:48:23 +0200

    Doc fix package casing

--- experimental/ast/predeclared/predeclared.go
+++ experimental/ast/predeclared/predeclared.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// package predeclared provides all of the identifiers with a special meaning
+// Package predeclared provides all of the identifiers with a special meaning
 // in Protobuf.
 //
 // These are a subset of the [keyword.Keyword] enum which are names that are
